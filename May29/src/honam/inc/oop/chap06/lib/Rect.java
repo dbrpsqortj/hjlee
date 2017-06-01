d6aae12b8b5d3be0b8c4bd2a2164fab0ca5767f3
owner <owner@owner-PC>
2017-06-01 10:18:40 +0900

    6월 1일 수업시간 6장 실습

---
+++ May29/src/honam/inc/oop/chap06/lib/Rect.java
@@ -0,0 +1,29 @@
+package honam.inc.oop.chap06.lib;
+
+public class Rect {
+	int width;
+	int height;
+
+	public Rect() {
+
+	}
+
+	public Rect(int width, int height) {
+		this.width = width;
+		this.height = height;
+
+	}
+
+	public boolean equals(Rect p) {
+		if (width * height == p.height * p.width) {
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+
+	public String toString() {
+		return "Rect";
+	}
+}
